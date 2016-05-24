e1b02d44c653722305bc3209b8172a8fcb33ac2b
Carlo Tomasin <c.tomasin@gmail.com>
2016-05-24 23:26:11 +0200

    piccole modifiche

--- remote/robottino-video.sh
+++ remote/robottino-video.sh
@@ -4,7 +4,7 @@ source envsetup
 
 #while true; do
   echo accepting video streaming
-  nc -k -l $PORT_VID | mplayer -fps 200 -demuxer h264es -
+  nc -k -l $PORT_VID | mplayer -noconsolecontrols -fps 200 -demuxer h264es -
 #  sleep 2
 #done
 
