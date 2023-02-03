0c2fdca521b855bf44a64d5b5b5f36a0f250688a
Carlo Tomasin <c.tomasin@gmail.com>
2023-02-03 23:33:05 +0000

    check performed

--- robottino.py
+++ robottino.py
@@ -2,6 +2,8 @@
 
 import RPi.GPIO as GPIO
 import time
+import sys
+import asyncio
 
 GPIO.setmode(GPIO.BCM)
 
@@ -15,16 +17,15 @@ class Motor:
     self.pwm = pwm
     self.sf = sf
     self.pwm_handle = self.gpio_init()
-    self.check()
 
   def __del__(self):
     self.gpio_deinit()
 
   def gpio_init(self):
-    GPIO.setup( (self.en, self.dir), GPIO.OUT)
+    GPIO.setup( (self.en, self.dir, self.pwm), GPIO.OUT)
     if self.sf: GPIO.setup(self.sf, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     pwm = GPIO.PWM(self.pwm, Motor.PWM_FREQUENCY_HZ)
-    pwm.start(0)
+    pwm.start(0.0)
     return pwm
 
   def gpio_deinit(self):
@@ -33,23 +34,34 @@ class Motor:
     GPIO.cleanup((self.en, self.dir, self.pwm))
     if self.sf: GPIO.cleanup(self.sf)
 
-  def check(self):
+  async def check(self):
+    print(f"checking motor {self.num}")
+    self.pwm_handle.start(50.0)
+    GPIO.output(self.en, GPIO.HIGH)
+    GPIO.output(self.dir, GPIO.HIGH)
+    await asyncio.sleep(1.0)
+    GPIO.output(self.dir, GPIO.LOW)
+    await asyncio.sleep(1.0)
+    GPIO.output(self.en, GPIO.LOW)
+    self.pwm_handle.stop()
     if self.sf:
-      GPIO.output(self.en, GPIO.HIGH)
-      time.sleep(0.1)
-      print("Motor controller {} is on").format(self.num) if not GPIO.input(self.sf) else print("Motor controller {} ERROR!").format(self.num)
-      GPIO.output(self.en, GPIO.LOW)
+      print(f"Motor controller {self.num} is on") if GPIO.input(self.sf) else print("Motor controller {self.num} ERROR!")
+    print(f"checking motor {self.num} ended")
 
-m1 = Motor(1, 22, 24, 12, sf = 5)
-m2 = Motor(2, 23, 25, 13, sf = 6)
 
-import sys
+async def main():
+  m1 = Motor(1, 22, 24, 12, sf = 5)
+  m2 = Motor(2, 23, 25, 13, sf = 6)
+  await asyncio.gather(
+    m1.check(),
+    m2.check()
+  )
 
 try:
+  asyncio.run(main())
   while True:
     time.sleep(0.1)
 except KeyboardInterrupt:
-    del m1
-    del m2
     print("Bye")
+    GPIO.cleanup()
     sys.exit(0)
