93d2e5af49908045fa14d2a109ada1c7e485ef89
Carlo Tomasin <carlo.tomasin@icare-world.com>
2023-02-10 13:58:22 +0100

    testing in non-rpi env

--- poc/opencv2html.py
+++ poc/opencv2html.py
@@ -7,20 +7,13 @@ import flask
 
 web_app = flask.Flask(__name__,
                       template_folder='.')
-camera = cv2.VideoCapture('libcamerasrc')
-
-print(camera.isOpened())
-print(camera.grab())
-print(camera.read())
 
 def gen_frames():
   while True:
-      success, frame = camera.read()
-      if not success:
-          print('cannot read frame from camera')
-          break
+      frame = cv2.imread('sample.jpg')
       ret, buffer = cv2.imencode('.jpg', frame)
       frame = buffer.tobytes()
+      print(f'sending frame ({len(frame)} B)')
       yield (b'--frame\r\n'
              b'Content-Type: image-jpeg\r\n\r\n' + frame + b'\r\n')
       
@@ -30,9 +23,11 @@ def index():
 
 @web_app.route('/video_feed')
 def video_feed():
-   return flask.Response(gen_frames(),
+   rsp = flask.Response(gen_frames(),
                          mimetype='multipart/x-mixed-replace; boundary=frame')
+   print(f'encoding: {rsp.stream.encoding}')
+   return rsp
 
 if __name__ == '__main__':
-   web_app.run(debug=True, host='0.0.0.0')
+   web_app.run(debug=False, host='0.0.0.0')
 
